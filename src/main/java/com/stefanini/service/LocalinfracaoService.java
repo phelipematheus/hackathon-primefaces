d28c7c0df68c0356e7656126eabc5fe4b080215c
phelipematheus <phelipematheus@gmail.com>
2016-11-09 10:35:23 -0200

    localInfracao foi implementado e feio o fluxo corretamente

--- src/main/java/com/stefanini/service/LocalinfracaoService.java
+++ src/main/java/com/stefanini/service/LocalinfracaoService.java
@@ -1,5 +1,36 @@
 package com.stefanini.service;
 
-public class LocalinfracaoService {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import com.stefanini.model.Localinfracao;
+import com.stefanini.repository.LocalifracaoRepository;
+
+@Stateless
+public class LocalinfracaoService {
+	
+	@Inject
+    private LocalifracaoRepository localInfracaoRepository;
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void incluir(Localinfracao localInfracao){
+    	this.localInfracaoRepository.incluir(localInfracao);
+    }
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public List<Localinfracao> lista(){
+    	return this.localInfracaoRepository.lista();
+    }
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void excluir(Localinfracao localInfracao){
+    	this.localInfracaoRepository.excluir(localInfracao);
+    }
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void editar(Localinfracao localInfracao){
+    	this.localInfracaoRepository.incluir(localInfracao);
+    }
+	
 }
